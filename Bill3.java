c43fcce9f5cfa22e862e3f7ea5cd44b732983967
Shaul Bar-Lev <shaulbarlev1@gmail.com>
2025-10-28 18:34:01 +0200

    finish Bill3

--- Bill3.java
+++ Bill3.java
@@ -3,6 +3,13 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    String name1 = args[0];
-	    // Replace this comment with the rest of your code   
+		String name2 = args[1];
+		String name3 = args[2];
+		double sum = Double.parseDouble(args[3]);
+
+		double each = Math.ceil(sum / 3);
+		String message = name1 + ", " + name2 + " and " + name3 + ": Pay " + each + " each";
+
+		System.out.println(message);
 	}
 }
