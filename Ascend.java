45b415ac71dd9af62a6e111d8892f7fe93987306
Shaul Bar-Lev <shaulbarlev1@gmail.com>
2025-10-29 15:48:29 +0200

    finish Ascend

--- Ascend.java
+++ Ascend.java
@@ -3,5 +3,23 @@
 public class Ascend {
 	public static void main(String[] args) {
 		// Replace this comment with your code
+
+		int lim = Integer.parseInt(args[0]);
+		
+		int a = (int) (Math.random()*lim);
+		int b = (int) (Math.random()*lim);
+		int c = (int) (Math.random()*lim);
+
+		int min = Math.min(Math.min(a,b),c);
+		int max = Math.max(Math.max(a,b),c);
+		
+		int mid = (a+b+c)-min-max;
+
+		System.out.println(a + " " + b + " " + c);
+		System.out.println(min + " " + mid + " " + max);
+
+
+
+
 	}
 }
