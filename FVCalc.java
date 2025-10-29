eedcb59f254fb79a6968144632637a33d5d5a2a2
Shaul Bar-Lev <shaulbarlev1@gmail.com>
2025-10-29 16:16:15 +0200

    fix FVCalc

--- FVCalc.java
+++ FVCalc.java
@@ -8,7 +8,7 @@ public class FVCalc {
 		double futureValue = currentValue * Math.pow(1+rate/100,n);
 
 		// System.out.println((int) futureValue);
-		System.out.println("After " + n + " years, $" + currentValue + " saved at " + rate + "% will yield $" + (int) futureValue);
+		System.out.println("After " + n + " years, $" + (int) currentValue + " saved at " + rate + "% will yield $" + (int) futureValue);
 
 		//After 2 years, $100 saved at 10.0% will yield $121
 	}
