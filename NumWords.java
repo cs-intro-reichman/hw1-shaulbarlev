c8d63fb918283d02d5a799023f61e22d21601f47
Shaul Bar-Lev <shaulbarlev1@gmail.com>
2025-10-29 16:32:06 +0200

    finish NumWords

--- NumWords.java
+++ NumWords.java
@@ -2,5 +2,15 @@
 public class NumWords {
 	public static void main(String args[]) {
 	    // Replace this comment with your code
+		int input = Integer.parseInt(args[0]);
+		int hundreds = input/100;
+		int tens = (input%100)/10;
+		int ones = (input%10);
+
+		// System.out.println(hundreds);
+		// System.out.println(tens);
+		// System.out.println(ones);
+
+		System.out.println(hundreds + " hundreds, " + tens + " tens, and " + ones + " ones.");
 	}
 }
